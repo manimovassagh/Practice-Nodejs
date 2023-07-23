d451b08be678dc710b4ea598465b79b1bed6d8b1
Mani Movassagh <mani.mgh@gmail.com>
2023-07-23 02:07:42 +0200

    cleanup

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,17 @@ import express from 'express';
 import { Express,Request, Response} from 'express';
 
 
-
 const app:Express = express();
 
 
 app.use(express.static('public'))
-app.get('/test', (req:Request, res:Response) => {
+app.get('/test', (_req:Request, res:Response) => {
     res.json({
         name: 'John',
     });
 
 })
-app.get('/user', (req:Request, res:Response) => {
+app.get('/user', (_req:Request, res:Response) => {
     res.json({
         name: 'John',
     });
